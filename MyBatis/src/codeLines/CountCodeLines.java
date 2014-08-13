94d3427393bd6599036579c1d5be8ee13c979121
student <student@itcwin.com>
2014-08-13 17:17:57 +0400

    codeLines

--- MyBatis/src/codeLines/CountCodeLines.java
+++ MyBatis/src/codeLines/CountCodeLines.java
@@ -1,12 +1,39 @@
 package codeLines;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class CountCodeLines {
 
-	File file = new File("");
-	Scanner scanner = new Scanner(System.in);
+	File file = new File("/Users/student/Documents/CodeKata/13/textSmall");
+	// FileInputStream fileInput = new FileInputStream(file.);
 	int count = 0;
-	
+	String textLine;
+	Scanner scannerText;
+
+	public void countingLines() {
+
+		try {
+			scannerText = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		while (scannerText.hasNextLine()) {
+			textLine = scannerText.nextLine();
+				
+			
+			System.out.println(textLine);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		CountCodeLines lines = new CountCodeLines();
+		lines.countingLines();
+	}
 }
+
+//			char[] ch = textLine.toCharArray();
+//			count++
