7a6ffe9f5686f31a45bda6ab62669a6e997632c2
student <student@itcwin.com>
2014-08-21 16:13:14 +0400

    ohh..(

--- MyBatis/src/codeLines/CountCodeLines.java
+++ MyBatis/src/codeLines/CountCodeLines.java
@@ -3,14 +3,18 @@ package codeLines;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class CountCodeLines {
 
 	File file = new File("/Users/student/Documents/CodeKata/13/textBig");
 	Scanner scannerText;
 	String textLine;
+	StringTokenizer token;
 	int count = 0;
-
+	char[] ch;
+	String string = "*/";
+	
 	public void countingLines() {
 		try {
 			scannerText = new Scanner(file);
@@ -19,10 +23,16 @@ public class CountCodeLines {
 		}
 		while (scannerText.hasNextLine()) {
 			textLine = scannerText.nextLine().trim();
-			boolean comment = textLine.startsWith("/*")
-					|| textLine.startsWith("//") || textLine.endsWith("*/")
+//			token = new StringTokenizer(textLine, "");
+//			token.;
+			boolean comment = 
+					 textLine.startsWith("//") 
+//					 textLine.startsWith("/*"); 
+//					|| textLine.endsWith("*/") 
 					|| textLine.startsWith("*") || textLine.isEmpty();
-			if (!comment) {
+			ch = textLine.toCharArray();
+			
+			if (!comment){
 				count++;
 				System.out.println(textLine);
 			}
